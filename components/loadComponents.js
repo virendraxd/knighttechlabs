abf4e5845839b619c7c0b6bbe2d5d32333498c94
virendraxd <shakyaraju605@gmail.com>
2026-02-13 15:13:02 +0530

    changes

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -2,18 +2,13 @@ async function loadComponent(id, file) {
   const el = document.getElementById(id);
   if (!el) return;
 
-  const res = await fetch(file);
+  const res = await fetch("/knighttechlabs/" + file);
   el.innerHTML = await res.text();
 }
 
 async function loadAll() {
-  await loadComponent("header", "../../components/header.html");
-  await loadComponent("footer", "../../components/footer.html");
-
-  // Now safe to run page scripts
-  if (typeof initPage === "function") {
-    initPage();
-  }
+  await loadComponent("header", "components/header.html");
+  await loadComponent("footer", "components/footer.html");
 }
 
 loadAll();
