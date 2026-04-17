421c675a63b823c1f29970bea28ad3f4c05a290c
virendraxd <shakyaraju605@gmail.com>
2026-04-17 23:22:06 +0530

    key changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const admin = require("firebase-admin");
 // =============================
 // 🔐 FIREBASE ADMIN SETUP
 // =============================
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
