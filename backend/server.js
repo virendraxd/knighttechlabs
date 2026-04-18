e76ca78ca64af277a036b08d4bcda81f7216d6bb
virendraxd <shakyaraju605@gmail.com>
2026-04-18 22:06:10 +0530

    set max count to 3

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.post("/check-download", async (req, res) => {
     }
 
     // limit reached
-    if (count >= 5) {
+    if (count >= 3) {
       return res.json({ allowed: false, count });
     }
 
